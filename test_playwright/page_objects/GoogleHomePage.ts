b2d0cb8bfaffaa42682f59f16a71d3a021c3a488
minhvl1 <minhvu890yahoo@gmail.com>
2022-11-18 08:46:31 +0700

    update alias script

--- test_playwright/page_objects/GoogleHomePage.ts
+++ test_playwright/page_objects/GoogleHomePage.ts
@@ -1,10 +1,13 @@
 export class GoogleHomePage{
 
-    get getSearchInput(){
-        return "//input[@name='q']"
-    }
+    // get getSearchInput(){
+    //     return "//input[@name='q']"
+    // }
+    //
+    // get expectURL(){
+    //    return "https://www.google.com/search?q=minh&s"
+    // }
+    public getSearchInput ="//input[@name='q']"
+    public expectURL="https://www.google.com/search?q=minh&s"
 
-    get expectURL(){
-       return "https://www.google.com/search?q=minh&s"
-    }
 }
