2977621c0b9310d7e516befada11665f5169f79f
minhvl1 <minhvu890yahoo@gmail.com>
2022-12-07 13:38:19 +0700

    shadowroot

---
+++ test_playwright/steps/ShadowRoot.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect, Page} from '@playwright/test';
+
+
+test.beforeAll(async ()=>{
+
+})
+
+test.describe('shadow root',  () => {
+    test.only('selectorhub',async ({page})=>{
+        await page.goto("https://selectorshub.com/xpath-practice-page/")
+
+        let username = "#userName #kils"
+        // let username ="input#kils"
+
+        let pizza = "#userName #app2 #pizza"
+        // let pizza = "input#pizza"
+
+        await page.fill(username,"minh")
+        await page.fill(pizza,"PIZZA")
+        await page.waitForTimeout(5000)
+
+
+    })
+
+    })
